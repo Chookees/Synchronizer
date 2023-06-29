d7e7bb1d42d1679f2f3973ac727805cc2c9f8790
Artur Zubert-Bobb <arturzubert@web.de>
2023-06-29 09:21:59 +0200

    namespace adjustment

--- Synchronizer/Program.cs
+++ Synchronizer/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Synchronizer
+namespace az.Synchronizer
 {
     internal static class Program
     {
